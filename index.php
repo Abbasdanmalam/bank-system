d103d3ad62556828a399b806c6dd2e6a677e3c8a
jtea9 <muhammadjalfaki9@gmail.com>
2023-06-17 01:48:38 -0700

    added bootstrap

--- index.php
+++ index.php
@@ -1,62 +1,130 @@
 <?php
-
 $conn = mysqli_connect("localhost", "root", "");
 
 mysqli_select_db($conn, "bank");
 
-// $First_Name = "AL-FAKI";
+if(isset($_POST['register'])) {
 
-// // print $First_Name;
-// // print "<br>";
-// $Last_Name = "Jubril";
-// echo $Last_Name." ".First_Name;
-// echo "he said "."he doesn.'t like computer";
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $pass = md5($_POST['password']);
+    $accountNumber = rand();
+    $fullname = $firstname." ".$lastname;
 
-if(isset($_POST['depositBtn'])){
+    $query = mysqli_query($conn, "insert into customers (fullname, accountNumber, balance, EMAIL, PASSWORD) 
+    values('$fullname','$accountNumber','0', '$email', '$pass')") or die(mysqli_error($conn));
 
-    $deposit = $_POST["deposit"];
-    $accountNumber = $_POST['accountNumber'];
-$getcustomer = mysqli_query($conn, "select * from customers where accountNumber = '$accountNumber'");
-
-$count = mysqli_num_rows($getcustomer);
-
-if($count == 1){
-while($row = mysqli_fetch_assoc($getcustomer)) {
-
-    $fullname = $row['fullname'];
-    $balance = $row['balance'];
-
-    $newBalance = $balance + $deposit;
-
-    $savedeposit = mysqli_query($conn, "insert into deposits (accountNumber, amount) 
-    values('$accountNumber', '$deposit')");
-
-    $updateBalance = mysqli_query($conn, "update customers set balance = '$newBalance' 
-    where accountNumber ='$accountNumber'");
-
-    if($updateBalance) {
-        echo "Hello ".$fullname." Your Deposit of UGX ".$deposit." on Account Number ".$accountNumber."
-         was successful your new balance is ".$newBalance;
+    if($query) {
+header("location:login.php");
     }
+
+
+
 }
 
-} else {
-    echo "Invalid Account Number";
-}
-
-
-
-
-
-// $CustomersBalance = $CustomersBalance + $deposit;
-
-// print "Your deposit of ".$CustomersBalance." UGX was sucessfully";
-
-}
 ?>
 
-<form action="" method="post">
-<input type="text" name="accountNumber" placeholder="Enter Your Account Number"/>
-<input type="text" name="deposit" placeholder="Enter the deposit Ammount"/>
-<input type="submit" name="depositBtn" value="Deposit">
-</form>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Internship Bank</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                    <div class="col-lg-7">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                            </div>
+                            <form class="user" action="" method="post">
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="text" name="firstname" class="form-control form-control-user" id="exampleFirstName"
+                                            placeholder="First Name">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="text" name="lastname" class="form-control form-control-user" id="exampleLastName"
+                                            placeholder="Last Name">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail"
+                                        placeholder="Email Address">
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="password" name="password" class="form-control form-control-user"
+                                            id="exampleInputPassword" placeholder="Password">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="password" class="form-control form-control-user"
+                                            id="exampleRepeatPassword" placeholder="Repeat Password">
+                                    </div>
+                                </div>
+                                <input type="submit" name="register" value="Create Account" class="btn btn-primary btn-user btn-block">
+                                    
+                                <hr>
+                                <!-- <a href="index.html" class="btn btn-google btn-user btn-block">
+                                    <i class="fab fa-google fa-fw"></i> Register with Google
+                                </a>
+                                <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                    <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
+                                </a> -->
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                            </div>
+                            <div class="text-center">
+                                <a class="small" href="login.html">Already have an account? Login!</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
---
+++ steven.php
@@ -0,0 +1,9 @@
+<?php
+for($distance = 50;$distance<=250;$distance +=50){
+    echo "<tr>
+    <td style=\"text-align: right;\">".($distance."</td>
+    <td style=\"text-Align;right;">"".($distance/10)."</td>
+    </tr>\n";
+}
+
+?>
